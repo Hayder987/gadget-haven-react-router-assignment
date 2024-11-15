1272fd382b1e70d9be4963a12e1010335352b5bf
hayder <hayderbd4290@gmail.com>
2024-11-15 09:26:16 +0600

    remote private to public

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import {
 } from "react-router-dom";
 import { router } from './routes/router';
 import { ToastContainer } from 'react-toastify';
-  import 'react-toastify/dist/ReactToastify.css';
+import 'react-toastify/dist/ReactToastify.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
